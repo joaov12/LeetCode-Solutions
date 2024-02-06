e3b48e65f5cd5d159558207445dead5a3b8a9bc1
João Vitor Soares <jvfs12th@gmail.com>
2024-02-06 13:26:24 -0300

    :rocket: LeetCode: Two Sum

---
+++ LeetCode/Two_Sum.cs
@@ -0,0 +1,16 @@
+public static int[] TwoSum(int[] nums, int target)
+{
+    int n = nums.Length;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return [i,j];
+            }
+        }
+    }
+    return [];
+}
