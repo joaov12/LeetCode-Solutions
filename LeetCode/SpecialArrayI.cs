f02bfcaabb30b3493ee3c9c490af73fc47f74658
João Vitor Farias Soares <51096648+joaov12@users.noreply.github.com>
2024-05-20 14:07:32 -0300

    Create SpecialArrayI.cs

---
+++ LeetCode/SpecialArrayI.cs
@@ -0,0 +1,16 @@
+  // Runtime 71 ms
+  // Memory 44.9 MB
+
+  public bool IsArraySpecial(int[] nums) {
+        if (nums.Length <= 1){
+            return true;
+        }
+        
+        for (int i = 0; i < nums.Length - 1; i++){
+            if ((nums[i] % 2) == (nums[i + 1] % 2)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
